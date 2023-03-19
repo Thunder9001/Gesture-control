c623659722f673cf351b60ae5d3aadb0f2a1af67
Daniel <30436382+Thunder9001@users.noreply.github.com>
2023-03-19 12:56:19 +0000

    Main Menu UI
    Added interactable buttons for Main Menu

---
+++ Gesture Game/Assets/HandMenuButtons.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HandMenuButtons : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Tutorial()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
