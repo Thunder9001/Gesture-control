b3103d8037b739713a292686de088d32147acbce
Daniel <30436382+Thunder9001@users.noreply.github.com>
2023-04-23 19:39:25 +0100

    Added slicing code and Fruit assets
    Added and implemented initial cutting code and imported fruit models into Unity.
    Next: Refine implemented gestures and finish tutorial

--- Gesture Game/Assets/Scripts/Interaction/GestureInteraction.cs
+++ Gesture Game/Assets/Scripts/Interaction/GestureInteraction.cs
@@ -8,11 +8,6 @@ using Leap;
 public class GestureInteraction : MonoBehaviour
 {
     public LeapProvider leapProvider;
-    //Direction of Push and pull
-    Vector3 rHandDirection;
-    Vector3 rIndexFingerDirection;
-    Hand _rightHand;
-
 
     // Create a list of game objects one for each interaction.
     public List<GameObject> pushList;
@@ -23,36 +18,60 @@ public class GestureInteraction : MonoBehaviour
     //Controlled by Leap detectors
     bool push = false;
     bool pull = false;
+
+    //Get palm ui element
+
     public void Push()
     {
-        push = true;
+        if(CheckPalmUI())
+        {
+            push = false;
+        }
+        else
+        {
+            push = true;
+        }
     }
+   
     public void StopPush()
     {
         push = false;
     }
+    
     public void StopPull()
     {
         pull = false;
     }
+    
     public void Pull()
     {
-        pull = true;
+        if (CheckPalmUI())
+        {
+            pull = false;
+        }
+        else
+        {
+            pull = true;
+        }
+    }
+
+    public bool CheckPalmUI()
+    {
+        return false;
     }
     // Start is called before the first frame update
     void Start()
     {
         pushList = new List<GameObject>();
         pullList = new List<GameObject>();
-        _rightHand = Hands.Right;
     }
 
     // Update is called once per frame
     void Update()
     {
-        _rightHand = Hands.Right;
-        rHandDirection = new Vector3(_rightHand.PalmNormal.x, _rightHand.PalmNormal.y, _rightHand.PalmNormal.z).normalized;
-        rIndexFingerDirection = _rightHand.GetIndex().Direction.normalized;
+        Hand _rightHand = Hands.Right;
+        Vector3 rHandDirection = new Vector3(_rightHand.PalmNormal.x, _rightHand.PalmNormal.y, _rightHand.PalmNormal.z).normalized;
+        Vector3 rIndexFingerDirection = _rightHand.GetIndex().Direction.normalized;
         if (push == true)
         {
             transform.Translate (rHandDirection * speed * Time.deltaTime, Space.World);
---
+++ Gesture Game/Assets/Scripts/Slicing/SliceListener.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Code sourced from https://github.com/LandVr/SliceMeshes
+// Permission to use and adapt given in https://youtu.be/iRW2CyQysdw
+public class SliceListener : MonoBehaviour
+{
+    //Check when the slicer is in contact with a slicable object.
+    public Slicer slicer;
+    private void OnTriggerEnter(Collider other)
+    {
+        slicer.isTouched = true;
+    }
+}
